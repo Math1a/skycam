401a3b040878803f076864bcd3a020b8c80655a1
Mathia Segre <mathia.segre@gmail.com>
2022-05-11 16:07:52 +0000

    Improvements, fixes, and added cooling control for QHY

--- skycam.m
+++ skycam.m
@@ -11,11 +11,11 @@
 % "SkyImage_yyyy:mm:dd-HH:MM:SS" in /home/ocs/skycam/ (subject to change).
 
 % Setup
-addpath('/home/ocs/matlab/matlab-gphoto-master/')
 % Set the working directory, this is where images will be saved
 projectdir = "/home/ocs/skycam/";
 dateformat = 'yyyy:mm:dd-HH:MM:SS';
 cd(projectdir);
+addpath('/home/ocs/matlab/skycam/')
 addpath('/home/ocs/')
 
 sun = celestial.SolarSys.get_sun;
@@ -40,23 +40,35 @@ while sunalt < -5 % Maybe add a way to break?
     % deg. below the horizon)
     if sunalt < -5
         tic % Start measiring time
+        if ~isempty(dir('capt0000.nef'))
+            movefile('capt0000.nef', projectdir + "OldImage_" + ...
+                datestr(now, dateformat) + ".nef");
+        end
         filenum = length(dir('capt*'));
         p.capture; % Capture an image
         % Wait untill the camera is idle again, and the new image is saved
-        while length(dir('capt*')) <= filenum; end
+        while length(dir('capt*')) <= filenum || isempty(p.lastImageFile); end
         waittime = toc; % Get the time it took to capture the image
         
         % Rename newly captured image to match date and time
-        movefile(string(p.lastImageFile), projectdir + "SkyImage_" + ...
-            datestr(now - (waittime/86400), dateformat) + ".nef");
+        % Unknown errors can occur, in that case, try again
+        try
+            filename = projectdir + "SkyImage_" +  datestr(now - (waittime/86400), dateformat) + ".nef";
+            movefile(string(p.lastImageFile), filename)
+        catch err
+            pause(3)
+            movefile(string(p.lastImageFile), filename)
+        end
+        disp("New image " + filename + " saved successfully!")
     end
     if waittime < 60
-    pause(60 - waittime)
+        pause(60 - waittime)
     end
     
     % Get an update on the sun's position
-    sun = celestial.SolarSys.get_sun;
-    sunalt = rad2deg(sun.Alt);
+    %sun = celestial.SolarSys.get_sun;
+    %sunalt = rad2deg(sun.Alt);
+    sunalt = sunalt + 1;
 end
 
 p.stop
