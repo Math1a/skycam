328e3cce53488c16d30912973aa71f9387f09fe8
Math1a <mathia.segre@gmail.com>
2022-05-19 16:18:41 +0000

    Exposure time, temperature and rewritten file save
    Exposure time now can be changed to one of the possible values on the
    list.
    You can now connect the arduino data logger to get temperature readings.
    Adjusted the way the script handels new files, now the script does not
    need to wait for p.lastImageFile
    The script is set to take images in a shorter interval, altough it is
    not optimized and it takes between 20 - 60 seconds to save the new image
    (Depending on how full the RAM is I think?).

--- connectSensor.m
+++ connectSensor.m
@@ -1,9 +1,11 @@
+%This script is to connect the Arduino data logger (if it exists)
 %Optional parameters:
 %'Port' - The port where the Arduino is connected, auto detect by deafult.
 %Example input: "/dev/ttyUSB0"
 %'Baud' - The baud rate (bits per second) of the communication, deafult is
 %9600.
 %
+clear S
 found = 0;
 
 if ~exist('Baud','var') || isempty(Baud)
@@ -30,3 +32,6 @@ else
     S = serialport(Port,Baud); % Set the port and the baud rate
     found = 1;
 end
+
+S.flush
+resp = S.readline;
--- skycam.m
+++ skycam.m
@@ -11,6 +11,19 @@
 % "SkyImage_yyyy:mm:dd-HH:MM:SS" in /home/ocs/skycam/ (subject to change).
 
 % Setup
+exptime = 8; % Exposure time, in seconds
+delay = 10; % The delay between each image, in seconds
+
+% Debug: Check if the temperature logger is connected (Arduino)
+connectSensor
+if found
+    disp("Temperature data logger detected!")
+    initialTemp = string(resp);
+    disp("Initial temperature: " + initialTemp)
+else
+    disp("No temperature data logger detected. Temperature will not be monitored")
+end
+
 % Set the working directory, this is where images will be saved
 projectdir = "/home/ocs/skycam/";
 dateformat = 'yyyy:mm:dd-HH:MM:SS';
@@ -18,9 +31,10 @@ cd(projectdir);
 addpath('/home/ocs/matlab/skycam/')
 addpath('/home/ocs/')
 
-sun = celestial.SolarSys.get_sun;
+% Get the sun's position
+%sun = celestial.SolarSys.get_sun;
 %sunalt = rad2deg(sun.Alt);
-sunalt = -100;
+sunalt = -10;
 
 % Initiate connection with camera
 p = gphoto;
@@ -31,44 +45,70 @@ else
     error("Could not find camera! Check connection")
 end
 
-plot(p);
+% Change exposure time, there is a list of possible values, the script will
+% automatically choose the closest one to the one selected in the beginning
+data = importdata("exptimes.txt");
+[val,idx] = min(abs(data-exptime));
+disp("Setting exposure time to closest available value:")
+p.set('bulb', 0)
+p.set('shutterspeed', idx-1)
 
+plot(p);
 pause(5)
 
+loop = 0;
 while sunalt < -5 % Maybe add a way to break?
     % This part of the loop will only activate at night (when the sun is 5
     % deg. below the horizon)
     if sunalt < -5
-        tic % Start measiring time
-        if ~isempty(dir('capt0000.nef'))
-            movefile('capt0000.nef', projectdir + "OldImage_" + ...
+        disp("Taking new image at " + datestr(now))
+        tic % Start measuring time
+        lastImageFile = strcat("capt" + sprintf('%04d',loop) + ".nef");
+        % Incomplete: Move old images out of the way, so they won't
+        % interfere with the new ones
+        if ~isempty(dir(lastImageFile))
+            movefile(lastImageFile, projectdir + "OldImage_" + ...
                 datestr(now, dateformat) + ".nef");
         end
         filenum = length(dir('capt*'));
         p.capture; % Capture an image
         % Wait untill the camera is idle again, and the new image is saved
-        while length(dir('capt*')) <= filenum || isempty(p.lastImageFile); end
-        waittime = toc; % Get the time it took to capture the image
+        while length(dir('capt*')) <= filenum; end
         
         % Rename newly captured image to match date and time
         % Unknown errors can occur, in that case, try again
+        waittime = toc; % Get the time it took to capture the image
         try
             filename = projectdir + "SkyImage_" +  datestr(now - (waittime/86400), dateformat) + ".nef";
-            movefile(string(p.lastImageFile), filename)
+            movefile(string(lastImageFile), filename)
         catch err
-            pause(3)
-            movefile(string(p.lastImageFile), filename)
+            pause(1)
+            filename = projectdir + "SkyImage_" +  datestr(now - (waittime/86400), dateformat) + ".nef";
+            movefile(string(lastImageFile), filename)
         end
         disp("New image " + filename + " saved successfully!")
+        disp("Elapsed time: " + string(waittime) +" seconds")
+        if found
+            S.flush
+            resp = S.readline;
+            disp("Temperature: " + string(resp))
+            loop = loop + 1;
+        end
     end
-    if waittime < 60
-        pause(60 - waittime)
+    if waittime < delay
+        pause(delay - waittime)
+    end
+    
+    if found && str2double(initialTemp) + 5 < str2double(resp)
+        p.stop
+        p.delete
+        error("Temperature too high!!! Let the camera cool down!")
     end
     
     % Get an update on the sun's position
     %sun = celestial.SolarSys.get_sun;
     %sunalt = rad2deg(sun.Alt);
-    sunalt = sunalt + 1;
+    %sunalt = sunalt + 1;
 end
 
 p.stop
